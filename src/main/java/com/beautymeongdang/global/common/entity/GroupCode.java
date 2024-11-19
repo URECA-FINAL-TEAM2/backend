e8e40b950289f55228c98001680ab6aa62a7fdd1
Minsu17 <tjalstn1119@naver.com>
2024-11-19 22:27:30 +0900

    init commit2

---
+++ src/main/java/com/beautymeongdang/global/common/entity/GroupCode.java
@@ -0,0 +1,24 @@
+package com.beautymeongdang.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupCode {
+
+    @Id
+    @Column(name = "group_code", length = 3)
+    private String groupCode;
+
+    @Column(name = "group_code_name", length = 20)
+    private String groupCodeName;
+
+    @Column(name = "group_code_desc", length = 500)
+    private String groupCodeDesc;
+}
---
+++ src/main/java/com/beautymeongdang/global/common/repository/CommonCodeRepository.java
@@ -0,0 +1,18 @@
+package com.beautymeongdang.global.common.repository;
+
+import com.beautymeongdang.global.common.entity.CommonCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommonCodeRepository extends JpaRepository<CommonCode, String> {
+    @Query("SELECT c FROM CommonCode c " +
+            "WHERE c.groupCode.groupCode = :groupCode " +
+            "AND c.commonCode = :commonCode")
+    Optional<CommonCode> findByGroupCodeAndCommonCode(
+            @Param("groupCode") String groupCode, @Param("commonCode") String commonCode);
+}
