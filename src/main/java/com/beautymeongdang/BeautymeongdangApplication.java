8659ce971533d4d86bd892faff0fb54549dc7a73
Minsu17 <tjalstn1119@naver.com>
2024-11-19 20:19:14 +0900

    init project

---
+++ src/main/java/com/beautymeongdang/BeautymeongdangApplication.java
@@ -0,0 +1,13 @@
+package com.beautymeongdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautymeongdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautymeongdangApplication.class, args);
+	}
+
+}
