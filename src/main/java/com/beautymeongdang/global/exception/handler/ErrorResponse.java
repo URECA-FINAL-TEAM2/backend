f126d346498449b264486212c1042e9dc55782a8
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-11-21 01:05:52 +0900

    init commit BaseException

---
+++ src/main/java/com/beautymeongdang/global/exception/handler/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.beautymeongdang.global.exception.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+public class ErrorResponse {
+
+    private HttpStatusCode status;
+    private String message;
+    private String timestamp;
+
+    public static ErrorResponse error(HttpStatusCode status, String message) {
+        return new ErrorResponse(status, message);
+    }
+
+    public ErrorResponse(HttpStatusCode status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = getCurrentTimestamp();
+    }
+
+    private static String getCurrentTimestamp() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+
+}
