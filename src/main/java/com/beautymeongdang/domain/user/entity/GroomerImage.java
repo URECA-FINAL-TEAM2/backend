28ba9c9ed33e3b727629e6aaad68e0fafa1658ad
KIMBUMSU123 <kbsg103@naver.com>
2024-11-21 13:25:50 +0900

    groomerImage

---
+++ src/main/java/com/beautymeongdang/domain/user/entity/GroomerImage.java
@@ -0,0 +1,27 @@
+package com.beautymeongdang.domain.user.entity;
+
+import com.beautymeongdang.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroomerImage extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groomerImageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "groomer_id",nullable = false)
+
+    private Groomer groomer;
+
+    private String imageUrl;
+    
+}
