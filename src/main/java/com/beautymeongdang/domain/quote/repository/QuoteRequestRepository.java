3a2a87ace25e471803c744d28733992ba21d8b5e
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-11-27 16:51:04 +0900

    Feat : 미용사 견적서 요청 공고 조회

--- src/main/java/com/beautymeongdang/domain/quote/repository/QuoteRequestRepository.java
+++ src/main/java/com/beautymeongdang/domain/quote/repository/QuoteRequestRepository.java
@@ -44,5 +44,39 @@ public interface QuoteRequestRepository extends JpaRepository<QuoteRequest, Long
            """)
     List<GroomerDirectRequestListResponseDto> findQuoteRequestsByGroomerId(@Param("groomerId") Long groomerId);
 
+    // 미용사 매장 근처 견적서 요청 공고 조회
+//    @Query(value = """
+//            SELECT new com.beautymeongdang.domain.quote.dto.GroomerDirectRequestListResponseDto(
+//                            qr.requestId,
+//                            u.nickname,
+//                            u.profileImage,
+//                            qr.createdAt,
+//                            qr.beautyDate,
+//                            d.dogBreed,
+//                            CAST(d.dogGender AS string),
+//                            d.dogWeight,
+//                            qr.content
+//                        )
+//                        FROM
+//                            QuoteRequest qr
+//                        LEFT JOIN
+//                            Quote q ON q.requestId = qr
+//                        JOIN
+//                            qr.dogId d
+//                        JOIN
+//                            d.customerId c
+//                        JOIN
+//                            c.userId u
+//                        JOIN
+//                            TotalQuoteRequest tqr ON tqr.requestId = qr
+//                        WHERE
+//                            q.requestId IS NULL
+//                          AND qr.isDeleted = false
+//                          AND qr.requestType = '010'
+//                          AND qr.status = '010'
+//                          AND tqr.sigunguId = :sigunguId
+//           """)
+//    List<GroomerDirectRequestListResponseDto> findQuoteRequestsBySigunguId(@Param("sigunguId") Long sigunguId);
+    // ToalQuoteRequest requestId PK
 
 }
