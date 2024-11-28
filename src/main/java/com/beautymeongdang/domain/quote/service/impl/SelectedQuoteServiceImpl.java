0f377dbe64eb221f76a5b78c6729e065eb9f4b3d
Minsu17 <tjalstn1119@naver.com>
2024-11-28 14:01:46 +0900

    Feat: 고객, 미용사 예약 상세 조회

--- src/main/java/com/beautymeongdang/domain/quote/service/impl/SelectedQuoteServiceImpl.java
+++ src/main/java/com/beautymeongdang/domain/quote/service/impl/SelectedQuoteServiceImpl.java
@@ -1,7 +1,10 @@
 package com.beautymeongdang.domain.quote.service.impl;
 
-import com.beautymeongdang.domain.quote.dto.CustomerSelectedQuoteResponseDto;
-import com.beautymeongdang.domain.quote.dto.GroomerSelectedQuoteResponseDto;
+import com.beautymeongdang.domain.quote.dto.GetCustomerSelectedQuoteResponseDto;
+import com.beautymeongdang.domain.quote.dto.GetSelectedQuoteDetailResponseDto;
+import com.beautymeongdang.domain.quote.dto.GetGroomerSelectedQuoteResponseDto;
+import com.beautymeongdang.domain.quote.entity.QuoteRequestImage;
+import com.beautymeongdang.domain.quote.repository.QuoteRequestImageRepository;
 import com.beautymeongdang.domain.quote.repository.SelectedQuoteRepository;
 import com.beautymeongdang.domain.quote.service.SelectedQuoteService;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +17,29 @@ import java.util.List;
 public class SelectedQuoteServiceImpl implements SelectedQuoteService {
 
     private final SelectedQuoteRepository selectedQuoteRepository;
+    private final QuoteRequestImageRepository quoteRequestImageRepository;
 
     @Override
-    public List<CustomerSelectedQuoteResponseDto> getSelectedQuotesForCustomer(Long customerId) {
+    public List<GetCustomerSelectedQuoteResponseDto> getSelectedQuotesForCustomer(Long customerId) {
         return selectedQuoteRepository.findCustomerSelectedQuotes(customerId);
     }
 
     @Override
-    public List<GroomerSelectedQuoteResponseDto> getSelectedQuotesForGroomer(Long groomerId) {
+    public List<GetGroomerSelectedQuoteResponseDto> getSelectedQuotesForGroomer(Long groomerId) {
         return selectedQuoteRepository.findGroomerSelectedQuotes(groomerId);
+    }
+
+    @Override
+    public GetSelectedQuoteDetailResponseDto getQuoteDetail(Long selectedQuoteId) {
+        GetSelectedQuoteDetailResponseDto mainData = selectedQuoteRepository.findQuoteDetailById(selectedQuoteId);
+
+        List<String> requestImages = quoteRequestImageRepository.findAllByRequestId(mainData.getQuoteId())
+                .stream()
+                .map(QuoteRequestImage::getImageUrl)
+                .toList();
+
+        mainData.setRequestImage(requestImages);
+
+        return mainData;
     }
 }
