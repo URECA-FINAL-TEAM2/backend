e5eeb5a00f8d6b58d1f8c8437e661a2d44bc8d31
Sieun53 <tldms1176@hanmail.net>
2024-11-29 17:26:21 +0900

    Feat: 시군구 조회 기능 구현

--- src/main/java/com/beautymeongdang/global/region/controller/RegionController.java
+++ src/main/java/com/beautymeongdang/global/region/controller/RegionController.java
@@ -2,10 +2,12 @@ package com.beautymeongdang.global.region.controller;
 
 import com.beautymeongdang.global.common.dto.ApiResponse;
 import com.beautymeongdang.global.region.dto.GetSidoResponseDto;
+import com.beautymeongdang.global.region.dto.GetSigunguResponseDto;
 import com.beautymeongdang.global.region.service.RegionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,6 @@ public class RegionController {
 
     private final RegionService regionService;
 
-
     /**
      * 시도 조회
      */
@@ -25,4 +26,16 @@ public class RegionController {
         GetSidoResponseDto response = regionService.getSidoList();
         return ApiResponse.ok(200, response, "시도 조회 성공");
     }
+
+
+    /**
+     * 시군구 조회
+     */
+    @GetMapping("/{sidoId}/sigungu")
+    public ResponseEntity<ApiResponse<GetSigunguResponseDto>> getSigunguList(
+            @PathVariable Long sidoId) {
+        GetSigunguResponseDto response = regionService.getSigunguList(sidoId);
+        return ApiResponse.ok(200, response, "Get Sigungu Success");
+    }
+
 }
