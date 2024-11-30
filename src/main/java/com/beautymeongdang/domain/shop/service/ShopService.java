d6eaf95c01e70e65733f9f85b2ecaa4d4acb5097
Sieun53 <tldms1176@hanmail.net>
2024-11-30 19:25:48 +0900

    Feat: 매장 삭제

--- src/main/java/com/beautymeongdang/domain/shop/service/ShopService.java
+++ src/main/java/com/beautymeongdang/domain/shop/service/ShopService.java
@@ -1,9 +1,6 @@
 package com.beautymeongdang.domain.shop.service;
 
-import com.beautymeongdang.domain.shop.dto.CreateShopRequestDto;
-import com.beautymeongdang.domain.shop.dto.CreateShopResponseDto;
-import com.beautymeongdang.domain.shop.dto.GetGroomerShopListResponseDto;
-import com.beautymeongdang.domain.shop.dto.GetShopDetailResponseDto;
+import com.beautymeongdang.domain.shop.dto.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ShopService {
@@ -14,7 +11,11 @@ public interface ShopService {
     // 매장 상세 조회
     GetShopDetailResponseDto.ShopDetailResponseDto getShopDetail(Long groomerId, Long customerId);
 
+    //미용사 삭제
+    DeleteShopResponseDto deleteShop(Long shopId);
+
     // 미용사 찾기 매장 리스트 조회
     GetGroomerShopListResponseDto.ShopListResponse getShopList(Long customerId);
+
 
 }
