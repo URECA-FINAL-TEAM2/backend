00a3cc8ccb814362040a9a027ee9243b322e454c
Sieun53 <tldms1176@hanmail.net>
2024-11-30 11:25:54 +0900

    Feat: 고객 메인 페이지 구현

--- src/main/java/com/beautymeongdang/domain/shop/repository/ShopRepository.java
+++ src/main/java/com/beautymeongdang/domain/shop/repository/ShopRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,22 @@ public interface ShopRepository extends JpaRepository<Shop, Long> {
     // grommerId를 통해 shop정보 찾기
     @Query("SELECT s FROM Shop s WHERE s.groomerId.groomerId = :groomerId")
     Optional<Shop> findByGroomerId(@Param("groomerId") Long groomerId);
+
+
+    // 미용사 평균 별점
+    @Query("SELECT COALESCE(AVG(r.starRating), 0) FROM Reviews r WHERE r.groomerId.groomerId = :groomerId AND r.isDeleted = false")
+    Double getAverageStarRatingByGroomerId(@Param("groomerId") Long groomerId);
+
+
+    // 우리동네 미용사 조회 (일단 최신순으로 두 개만 받아오기?)
+    @Query("""
+       SELECT s FROM Shop s 
+       JOIN FETCH s.groomerId g 
+       WHERE s.isDeleted = false 
+       AND s.sigunguId = (SELECT c.sigunguId FROM Customer c WHERE c.customerId = :customerId)  
+       ORDER BY s.createdAt DESC 
+       LIMIT 2
+       """)
+    List<Shop> findRecentLocalShopsByCustomerId(@Param("customerId") Long customerId);
+
 }
