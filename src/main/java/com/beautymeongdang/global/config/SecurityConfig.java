fbc68b7925c050a994d4bf9adfe553195b017d28
KIMBUMSU123 <kbsg103@naver.com>
2024-12-05 08:45:17 +0900

    getKakaoAccessToken 수정

--- src/main/java/com/beautymeongdang/global/config/SecurityConfig.java
+++ src/main/java/com/beautymeongdang/global/config/SecurityConfig.java
@@ -46,8 +46,7 @@ public class SecurityConfig {
                 // 기본 로그인 방식 비활성화
                 .formLogin((auth) -> auth.disable())
                 .httpBasic((auth) -> auth.disable())
-                .oauth2Login((auth) -> auth.disable())
-
+                .oauth2Login((auth) -> auth.disable()) // 커스텀 방식(프론트 인가 코드)을 사용하기 떄문에 사용 X
                 // 로그아웃 설정
                 .logout(logout -> logout
                         .logoutUrl("/api/auth/logout")
