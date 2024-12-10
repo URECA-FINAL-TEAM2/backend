7e84faae763121ee2594cdd1e1a0e8743d1f9394
Sieun53 <tldms1176@hanmail.net>
2024-12-11 02:35:34 +0900

    Feat: 채팅 메시지 보내기 구현

--- src/main/java/com/beautymeongdang/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/beautymeongdang/domain/chat/service/ChatMessageService.java
@@ -1,4 +1,12 @@
 package com.beautymeongdang.domain.chat.service;
 
-public class ChatMessageService {
+import com.beautymeongdang.domain.chat.dto.CreateChatMessageRequestDto;
+import com.beautymeongdang.domain.chat.dto.CreateChatMessageResponseDto;
+
+
+public interface ChatMessageService {
+
+    // 메시지 전송
+    CreateChatMessageResponseDto sendMessage(CreateChatMessageRequestDto messageRequestDto);
+
 }
