b3bd457dddb4fd5b899fac71e53f904ef165a432
Sieun53 <tldms1176@hanmail.net>
2024-12-11 16:29:42 +0900

    Feat: 본인 매장 상세 조회

--- src/main/java/com/beautymeongdang/domain/shop/dto/GetMyGroomerShopDetailResponseDto.java
+++ src/main/java/com/beautymeongdang/domain/shop/dto/GetMyGroomerShopDetailResponseDto.java
@@ -1,4 +1,43 @@
 package com.beautymeongdang.domain.shop.dto;
 
-public class GetMyGroomerShopDetailResponseDto{
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class GetMyGroomerShopDetailResponseDto {
+    private Long groomerId;
+    private Long shopId;
+    private String shopLogo;
+    private String shopName;
+    private Double starScoreAvg;
+    private Integer starCount;
+    private String address;
+    private String businessTime;
+    private String skills;
+    private Double latitude;
+    private Double longitude;
+    private Integer favoriteCount;
+    private String description;
+    private List<String> groomerPortfolioImages;
+    private String groomerUsername;
+    private String groomerProfileImage;
+    private List<ReviewDetailDto> reviews;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ReviewDetailDto {
+        private Long reviewId;
+        private String customerNickname;
+        private Double starScore;
+        private String content;
+        private Integer recommendCount;
+        private List<String> reviewsImage;
+        private LocalDateTime createdAt;
+    }
 }
