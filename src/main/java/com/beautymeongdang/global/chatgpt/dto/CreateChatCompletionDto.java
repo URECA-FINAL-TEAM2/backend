addae0f1f0ce9840f178ac00fa3ca654e83b8d25
KIMBUMSU123 <kbsg103@naver.com>
2024-12-14 21:53:00 +0900

    GPT 사용 환경 개선

--- src/main/java/com/beautymeongdang/global/chatgpt/dto/CreateChatCompletionDto.java
+++ src/main/java/com/beautymeongdang/global/chatgpt/dto/CreateChatCompletionDto.java
@@ -10,14 +10,16 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateChatCompletionDto {
-
     private String model;
-
     private List<CreateChatRequestMsgDto> messages;
+    private float temperature = 0.3f;    // 추가
+    private int max_tokens = 50;         // 추가
 
     @Builder
-    public CreateChatCompletionDto(List<CreateChatRequestMsgDto> messages) {
+    public CreateChatCompletionDto(List<CreateChatRequestMsgDto> messages, Float temperature, Integer max_tokens) {
         this.model = "gpt-3.5-turbo";
         this.messages = messages;
+        this.temperature = temperature != null ? temperature : 0.3f;
+        this.max_tokens = max_tokens != null ? max_tokens : 50;
     }
 }
