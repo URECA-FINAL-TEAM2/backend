602c7aeab1375e5a9d4e1800b6a28e929b911636
Sieun53 <tldms1176@hanmail.net>
2024-12-15 21:59:20 +0900

    Fix: 반려견 프로필 삭제 스케줄러

--- src/main/java/com/beautymeongdang/global/common/scheduler/dog/DogScheduledService.java
+++ src/main/java/com/beautymeongdang/global/common/scheduler/dog/DogScheduledService.java
@@ -1,11 +1,35 @@
 package com.beautymeongdang.global.common.scheduler.dog;
 
+import com.beautymeongdang.domain.dog.entity.Dog;
+import com.beautymeongdang.domain.dog.repository.DogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.beautymeongdang.infra.s3.FileStore;
+import java.time.LocalDateTime;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DogScheduledService {
+
+    private final DogRepository dogRepository;
+    private final FileStore fileStore;
+
+    // 반려견 물리적 삭제 스케줄러
+    @Scheduled(cron = "0 0 1 * * *")
+    @Transactional
+    public void deleteDog() {
+        List<Dog> dogs = dogRepository.findAllByIsDeletedAndAndUpdatedAt(LocalDateTime.now().minusDays(30));
+
+        dogs.forEach(dog -> {
+            if (dog.getProfileImage() != null) {
+                fileStore.deleteFile(dog.getProfileImage());
+            }
+            dogRepository.delete(dog);
+        });
+    }
 }
