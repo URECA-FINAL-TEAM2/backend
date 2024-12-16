19f22e30d6166540dc3bf98b6e8cfb0bed33c812
LeeDoRim <65598286+LeeDoRim@users.noreply.github.com>
2024-12-16 15:33:06 +0900

    Feat : 견적서 물리적 삭제 스케줄러

--- src/main/java/com/beautymeongdang/global/common/scheduler/quote/QuoteScheduledController.java
+++ src/main/java/com/beautymeongdang/global/common/scheduler/quote/QuoteScheduledController.java
@@ -1,6 +1,7 @@
 package com.beautymeongdang.global.common.scheduler.quote;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/scheduler/quote")
 @RequiredArgsConstructor
 public class QuoteScheduledController {
+
+    private final QuoteScheduledService quoteScheduledService;
+
+    // 견적서 물리적 삭제 스케줄러 테스트 컨트롤러
+//    @GetMapping("/delete-quote")
+//    public String deleteQuote() {
+//        quoteScheduledService.deleteQuote();
+//        return "Delete Quote Success";
+//    }
+
 }
--- src/main/java/com/beautymeongdang/global/common/scheduler/quote/QuoteScheduledService.java
+++ src/main/java/com/beautymeongdang/global/common/scheduler/quote/QuoteScheduledService.java
@@ -1,21 +1,34 @@
 package com.beautymeongdang.global.common.scheduler.quote;
 
+import com.beautymeongdang.domain.quote.entity.Quote;
+import com.beautymeongdang.domain.quote.repository.QuoteRepository;
 import com.beautymeongdang.domain.quote.service.SelectedQuoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class QuoteScheduledService {
     private final SelectedQuoteService selectedQuoteService;
+    private final QuoteRepository quoteRepository;
 
     // 예약 상태 미용 완료 변경
     @Scheduled(cron = "0 0 * * * *") //매 시간마다 실행
     public void updateExpiredQuotesStatus() {
         selectedQuoteService.updateStatusToCompletedIfPastBeautyDate();
+    }
+
+    // 견적서 물리적 삭제 스케줄러
+    @Scheduled(cron = "0 0 1 * * *")
+    public void deleteQuote() {
+        List<Quote> quotes = quoteRepository.findAllByIsDeletedAndUpdatedAt(LocalDateTime.now().minusDays(30));
+        quoteRepository.deleteAll(quotes);
     }
 
 }
