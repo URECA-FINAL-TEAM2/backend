e8c035cd56c1e31f8b699a5c0ff9d970e3a0c495
KIMBUMSU123 <kbsg103@naver.com>
2024-12-18 10:36:25 +0900

    Fix:토끼가 꼽줘서 수정

--- src/test/java/com/beautymeongdang/BeautymeongdangApplicationTests.java
+++ src/test/java/com/beautymeongdang/BeautymeongdangApplicationTests.java
@@ -1,13 +1,22 @@
 package com.beautymeongdang;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		RedisAutoConfiguration.class,
+		WebSocketServletAutoConfiguration.class
+})
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration,org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration"
+})
 class BeautymeongdangApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
